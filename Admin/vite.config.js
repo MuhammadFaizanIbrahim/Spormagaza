e11ac4315e0f7143403b5e6fad83b7284c6c7832
MuhammadFaizanIbrahim <mfaizanibrahim26@gmail.com>
2024-08-14 19:49:34 +0500

    Updated Vite.config.js for Admin

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
-    port: 4173,      // Use the desired port
+    port: 4174,      // Use the desired port
   }
 })
